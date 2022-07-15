25ec40fa26602ec74a45c593fcb39220a30cbb1d
Kdenn1 <103376676+Kdenn1@users.noreply.github.com>
2022-07-15 11:20:03 -0700

    added necessary files

---
+++ server.js
@@ -0,0 +1,7 @@
+//establish the required starting const declarations 
+const fs = require('fs');
+const express = require('express');
+const app = express();
+
+//establish the const that fetches the json data 
+const Notes = require('./miniature-eureka-main/Develop/db/db.json')
